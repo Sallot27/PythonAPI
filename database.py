958245118b5475dd8d0e121d6ca841b433fdb3b1
Sultan Bashammakh <sallot1000@outlook.com>
2025-07-28 09:49:26 +0300

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-from databases import Database
-
-DATABASE_URL = "sqlite:///./test.db"  # Use PostgreSQL or MySQL in production
-
-database = Database(DATABASE_URL)
-metadata = MetaData()
-engine = create_engine(DATABASE_URL)
